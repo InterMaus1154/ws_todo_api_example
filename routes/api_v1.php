3347dc1b7128348e45bef36b89eb84eeb95961b3
InterMaus1154 <amarco1154@gmail.com>
2025-09-15 15:20:15 +0100

    add db reset route

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,9 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-Route::get('/status', function(){
-   return response()->json([
+Route::get('/status', function () {
+    return response()->json([
         'message' => 'API is running:)'
-   ]);
-})->middleware('auth:sanctum');
+    ]);
+});
+
+Route::post('/db/reset', function () {
+    try {
+        Artisan::call('migrate:fresh --seed');
+        return response()->json([
+            'message' => 'Database has been reset',
+            'timeOfReset' => now()->toDateTimeString()
+        ]);
+    } catch (Throwable $e) {
+        return response()->json([
+            'message' => 'Database reset failed'
+        ], 500);
+    }
+});
